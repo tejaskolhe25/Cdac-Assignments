ff7243e006550b0898552b3bf3b463e7193bafb4
Tejas kolhe <120081032+tejaskolhe25@users.noreply.github.com>
2024-03-16 18:16:00 +0530

    Add files via upload

---
+++ OOPJ/Assignment 4/Assigmnet4.3/src/com/sample/Employee.java
@@ -0,0 +1,24 @@
+package com.sample;
+
+public class Employee {
+	
+	public void data(String name) {
+		System.out.println("Name of Employee:- "+name);
+	}
+	
+	public void data(int emp_id, int salary) {
+		
+		System.out.println("Employee id-       "+emp_id);
+		System.out.println("Employee Salary:-  "+salary);
+	}
+	
+	
+	public void data(double number,int age,String gender) {
+		System.out.println("Employee Number:- "+number);
+		System.out.println("Employee age:-      "+age);
+		System.out.println("Employee age:-      "+gender);
+		
+	}
+	
+	
+}
